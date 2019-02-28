aa13381ddd2d3416b49fe42a0648ceb849facbf1
Marek Hajdík <hajdik.marek1@gmail.com>
2019-02-28 14:52:33 +0100

    initial commit

---
+++ CV_1/__init__.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(np.random.uniform(0, 10))
+
+# inputX = np.arra
+
+weights = np.zeros(3)
+
+xAxis = np.arange(-25,25,1)
+plt.plot(xAxis, 2*xAxis+1)
+# plt.scatter // vykresli body
+plt.grid()
+plt.show()
+
