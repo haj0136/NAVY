87920063cd1a5b77cc2d9419c2624a35c81d28eb
maras49 <hajdk@seznam.cz>
2019-03-20 13:57:25 +0100

    neural network adjusted by hill climb

--- CV_3/__init__.py
+++ CV_3/__init__.py
@@ -1,4 +1,18 @@
 import numpy as np
+from CV_3.NeuralNetwork import NeuralNetwork
 
 if __name__ == '__main__':
-    LEARNING_RATE = 0.2
+    LEARNING_RATE = 0.001
+    NUMBER_OF_EPOCHS = 100000
+
+    nn = NeuralNetwork([2, 2, 1])
+
+    input_data = np.array([[0, 0], [0, 1],
+                           [1, 0], [1, 1]])
+    labels = np.array([0, 1,
+                       1, 0])
+
+    nn.train(input_data, labels, LEARNING_RATE, epochs=NUMBER_OF_EPOCHS)
+
+    for sample in input_data:
+        print(sample, nn.predict_single_data(sample))
