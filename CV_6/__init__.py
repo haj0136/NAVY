e8117dbf4f38fc720478f9018bee950c2bd00726
maras49 <hajdk@seznam.cz>
2019-04-13 15:20:47 +0200

    add levy dragon and hilbert's curve

--- CV_6/__init__.py
+++ CV_6/__init__.py
@@ -4,11 +4,16 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
 
     LENGTH = 2
-    ANGLE = np.pi/8
+    ANGLE = np.pi/2
     stack = []
 
+    dragonX = 'X+YF+'
+    dragonY = '-FX-Y'
+    hilbert_curveX = '-YF+XFX+FY-'
+    hilbert_curveY = '+XF-YFY-FX+'
 
-    def draw_fractal(actual_position, rule, angle, number_of_iterations):
+
+    def draw_fractal(actual_position, rule, angle, number_of_iterations, number_of_rewrites):
         actual_angle = angle
 
         for char in rule:
@@ -25,9 +30,15 @@ if __name__ == '__main__':
             if char == ']':
                 actual_position, actual_angle = stack.pop()
 
+            if char == 'X' and number_of_rewrites > 0:
+                actual_position, actual_angle = draw_fractal(actual_position, hilbert_curveX, actual_angle, number_of_iterations, number_of_rewrites - 1)
+
+            if char == 'Y' and number_of_rewrites > 0:
+                actual_position, actual_angle = draw_fractal(actual_position, hilbert_curveY, actual_angle, number_of_iterations, number_of_rewrites - 1)
+
             if char == 'F':
                 if number_of_iterations > 1:
-                    actual_position = draw_fractal(actual_position, rule, actual_angle, number_of_iterations - 1)
+                    actual_position = draw_fractal(actual_position, rule, actual_angle, number_of_iterations - 1, number_of_rewrites)
                 else:
                     new_position[0] = actual_position[0] + LENGTH * np.cos(actual_angle)
                     new_position[1] = actual_position[1] + LENGTH * np.sin(actual_angle)
@@ -35,17 +46,21 @@ if __name__ == '__main__':
                     plt.plot([actual_position[0], new_position[0]], [actual_position[1], new_position[1]])
                     actual_position = new_position
 
-        return actual_position
+        return actual_position, actual_angle
 
     first_angle = 0
     square = 'F+F-F-FF+F+F-F'
     snowflake = 'F+F--F+F'
     tree1 = 'F[+F]F[-F]F'
     tree2 = 'FF+[+F-F-F]-[-F+F+F]'
+    dragon = 'FX'
+    hilbert_curve = 'X'
+
+
     # plt.figure(dpi=200)
 
     first_position = [0, 0]
 
-    draw_fractal(first_position, tree2, first_angle, 3)
+    draw_fractal(first_position, hilbert_curve, first_angle, 1, 5)
     plt.axis("equal")
     plt.show()
